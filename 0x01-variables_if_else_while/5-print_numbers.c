5b350603d749f826d46499e82319abfffb7786b4
Raphaela664 <mahoraphy02@gmail.com>
2022-03-14 16:31:42 -0700

    Variables all done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,2 @@
+VimCrypt~03!"E¸•)Ÿº‚˜ÄÍ?–ç>|;ýE4VYs91?ˆ(Ó„‹µïD †Œ¦â«’6)€=‘™>g›,»N}œ“-I»433±×Ñ¦ØQ‰ÂXüyZ›h
+Ÿ‚—æWµ²rË©¢Rbf¾iÄcì…†]l†}v¢ÀÈèá¿¨€"“Žáj8GwoüÍÀà¦Âãî0å4S§–+ ¦õÖæ{ÇzBü×Äø:•ùAˆÑåîg«ž<Jid<@æÚØJœ`(“YÒ:‡Ø1 5€ÖÛ€³2àÊ	Œ”Ý´Q©
