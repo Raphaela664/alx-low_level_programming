09a3f14948035bf48fa62a1eccedbbdc7e16335a
Raphaela664 <mahoraphy02@gmail.com>
2022-03-21 15:07:33 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,5 +16,14 @@ int _atoi(char *s)
 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
+			sign *= -1;
+		i++;
 	}
-}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+	res *= sign;
+	return (res);
+
